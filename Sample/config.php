f42c2f7181bf91a942865708aeb89e7c17e81492
mateuszdolega <mdx@mdx.pl>
2015-08-06 15:34:58 +0200

    removed default config

--- Sample/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$wnBrandId = '';
-$wnMerchantApiUri = 'https://webnalist.com/api/v1';
-$wnMerchantAppPublicKey = '';
-$wnMerchantAppPrivateKey = '';
-$wnMerchantUsername = '';
-$wnMerchantPassword = '';
